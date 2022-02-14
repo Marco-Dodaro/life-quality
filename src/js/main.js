202a6ca2cf1c1f3ee13fb800aabcd56d07c3d150
Marco Dodaro <marco99dodaro@gmail.com>
2022-02-14 19:09:18 +0100

    js removed else

--- src/js/main.js
+++ src/js/main.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (city.indexOf(' ') > 0) {
         city = city.replace(/\s+/g, '-');
-      } else {}
+      }
 
       let apiUrl = apiUrlRoot + "slug:" + city + "/scores/";
 
@@ -33,43 +33,42 @@ document.addEventListener('DOMContentLoaded', () => {
         const categoriesDiv = document.querySelector(".displayCategories");
         const dataDiv = document.querySelector(".displayData");
 
-        if (data.summary == undefined) {
+        if (data.summary === undefined) {
           dataDiv.innerHTML = ``;
           categoriesDiv.innerHTML = ``;
           resultsDiv.classList.remove("displayResultsStyles", "flexCenter");
           searchBarDiv.innerHTML = `Invalid city name or city not found`;
           throw 'City not found';
-        } else {
+        }
 
-          searchBarDiv.innerHTML = ``;
+        searchBarDiv.innerHTML = ``;
 
         //  console.log (data);
-          let categories = data.categories;
-          resultsDiv.classList.add("displayResultsStyles", "flexCenter", "directionColumn");
+        let categories = data.categories;
+        resultsDiv.classList.add("displayResultsStyles", "flexCenter", "directionColumn");
 
-              const categoriesSection = `<div id="catgoriesSection" class="flexCenter directionColumn"><b>Categories:</b></div><br/>`;
-              categoriesDiv.innerHTML = categoriesSection;
-              resultsDiv.style = "align-items: unset"
+        const categoriesSection = `<div id="catgoriesSection" class="flexCenter directionColumn"><b>Categories:</b></div><br/>`;
+        categoriesDiv.innerHTML = categoriesSection;
+        resultsDiv.style = "align-items: unset"
 
-              for (let i = 0; i < categories.length; i++) {
-                let div = document.createElement("div");
-                div.innerHTML = "- " + categories[i].name + ": " + categories[i].score_out_of_10.toFixed(1) + `<br/>` ;
-                categoriesDiv.appendChild(div);
-              }
-
-          const html = `
-            <br/>
-            <div class="flexCenter directionColumn"><b>Summary:</b></div></br>
-            <div class="flexCenter directionColumn">${data.summary}</div></br>
-            <div class="flexCenter directionColumn"><b>Teleport city score:</b></div>
-            <div class="flexCenter directionColumn">${data.teleport_city_score.toFixed(0)}</div>
-          `;
-          dataDiv.innerHTML = html;
+        for (let i = 0; i < categories.length; i++) {
+          let div = document.createElement("div");
+          div.innerHTML = "- " + categories[i].name + ": " + categories[i].score_out_of_10.toFixed(1) + `<br/>` ;
+          categoriesDiv.appendChild(div);
         }
+
+        const html = `
+          <br/>
+          <div class="flexCenter directionColumn"><b>Summary:</b></div></br>
+          <div class="flexCenter directionColumn">${data.summary}</div></br>
+          <div class="flexCenter directionColumn"><b>Teleport city score:</b></div>
+          <div class="flexCenter directionColumn">${data.teleport_city_score.toFixed(0)}</div>
+        `;
+        dataDiv.innerHTML = html;
       }
     }
-  } catch (e) {
-    console.error('Error: ' + e);
+  } catch (err) {
+    console.error('Error: ' + err);
   }
 
 
